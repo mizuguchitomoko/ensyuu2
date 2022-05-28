7f0d952faf320dc5c238dd5a87a2ea137ae3331f
shuikouzhizi7@gmail.com <shuikouzhizi7@gmail.com>
2022-05-28 10:26:55 +0900

    プロジェクト ファイルを追加します。

---
+++ ensyuu2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ensyuu2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            while (i <= 10)//変数iが10まで繰り返す
+            {
+                if(i == 5){//変数iが5だった場合の処理
+                    MessageBox.Show(i.ToString());
+                    MessageBox.Show("ここで半分です");
+                    i++;
+                }
+                else//変数iが5以外の処理
+                {
+                    MessageBox.Show(i.ToString());
+                    i++;
+                }
+            }
+        }
+    }
+}
